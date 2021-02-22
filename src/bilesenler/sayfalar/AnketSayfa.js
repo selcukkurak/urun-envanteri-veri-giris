b24bddaffdbd291b24b6bfab9c948b86d2106401
Selçuk Kurak <slckkrk1996@gmail.com>
2021-02-22 15:43:37 +0300

    sayfalara yeni alanlar eklendi

--- src/bilesenler/sayfalar/AnketSayfa.js
+++ src/bilesenler/sayfalar/AnketSayfa.js
@@ -1,8 +1,12 @@
-import { Fragment } from 'react'
 import { AnketListe } from '../listeler'
+import { Route, Switch } from 'react-router-dom'
+import AnketEklemeForm from '../form/anket/AnketEklemeForm'
+import AnketGuncellemeForm from '../form/anket/AnketGuncellemeForm'
 
-export const AnketSayfa = () => (
-  <Fragment>
-    <AnketListe/>
-  </Fragment>
+export const AnketSayfa = ({ match }) => (
+  <Switch>
+    <Route path={`${match.url}/guncelle/:id`} component={AnketGuncellemeForm}/>
+    <Route path={`${match.url}/yeni-anket`} component={AnketEklemeForm}/>
+    <Route path="/anketler" component={AnketListe}/>
+  </Switch>
 )
--- src/bilesenler/sayfalar/IdariKayitSayfa.js
+++ src/bilesenler/sayfalar/IdariKayitSayfa.js
@@ -1,9 +1,14 @@
 import { Fragment } from 'react'
 import { OrtaBolme } from '../../App'
 import { IdariKayitListe } from '../listeler'
+import { Route, Switch } from 'react-router-dom'
+import IdariKayitEklemeFormu from '../form/idari-kayit/IdariKayitEklemeFormu'
 
-export const IdariKayitSayfa = () => (
+export const IdariKayitSayfa = ({match}) => (
   <Fragment>
-    <IdariKayitListe/>
+    <Switch>
+      <Route path={`${match.url}/yeni-idariKayit`} component={IdariKayitEklemeFormu}/>
+      <Route path="/idari-kayitlar" component={IdariKayitListe}/>
+    </Switch>
   </Fragment>
 )
