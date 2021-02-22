8d752650836a10b735b4ffd259e8a7e12ea11ac9
Selçuk Kurak <slckkrk1996@gmail.com>
2021-02-22 15:59:56 +0300

    gereksiz bağımlılıklar kaldırıldı.

--- src/bilesenler/sayfalar/IdariKayitSayfa.js
+++ src/bilesenler/sayfalar/IdariKayitSayfa.js
@@ -1,14 +1,10 @@
-import { Fragment } from 'react'
-import { OrtaBolme } from '../../App'
 import { IdariKayitListe } from '../listeler'
 import { Route, Switch } from 'react-router-dom'
 import IdariKayitEklemeFormu from '../form/idari-kayit/IdariKayitEklemeFormu'
 
-export const IdariKayitSayfa = ({match}) => (
-  <Fragment>
-    <Switch>
-      <Route path={`${match.url}/yeni-idariKayit`} component={IdariKayitEklemeFormu}/>
-      <Route path="/idari-kayitlar" component={IdariKayitListe}/>
-    </Switch>
-  </Fragment>
+export const IdariKayitSayfa = ({ match }) => (
+  <Switch>
+    <Route path={`${match.url}/yeni-idariKayit`} component={IdariKayitEklemeFormu}/>
+    <Route path="/idari-kayitlar" component={IdariKayitListe}/>
+  </Switch>
 )
