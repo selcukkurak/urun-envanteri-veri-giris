5f5db1629d7999cbbeb3cf300916ff94bee3d649
Selçuk Kurak <slckkrk1996@gmail.com>
2021-02-25 14:37:50 +0300

    anket formu güncellendi.

--- src/bilesenler/form/anket/AnketForm.js
+++ src/bilesenler/form/anket/AnketForm.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import { Field, Form, Formik } from 'formik'
 import { Container, Row, Col } from 'react-grid-system'
-import { Button, FormGroup, InputGroup } from '@blueprintjs/core'
+import { Button, FormGroup, InputGroup, Switch } from '@blueprintjs/core'
 import styled from 'styled-components'
 import { useRecoilValue } from 'recoil'
 import { referanslarState } from '../../store'
 import SelectField from '../SelectField'
-import { durumlar } from '../../hook/ortak'
 
 const Wrapper = styled.div`
   padding: 70px 0;
 `
 const Satir = styled(Row)`
-  padding:4px 8px;
+  padding: 4px 8px;
 `
-export default function AnketForm ({seciliAnket, history}) {
+export default function AnketForm ({ seciliAnket, history }) {
   const periyotlar = useRecoilValue(referanslarState).PERIYOT
-  const periyotOption = periyotlar && periyotlar.length !== 0 && periyotlar.map(periyot => ({label:periyot.adi, value:periyot.id}))
+  const periyotOption = periyotlar && periyotlar.length !== 0 && periyotlar.map(periyot => ({
+    label: periyot.adi,
+    value: periyot.id
+  }))
 
   const cografiDuzeyler = useRecoilValue(referanslarState).COGRAFI_DUZEY
-  const cografiDuzeyOption =cografiDuzeyler && cografiDuzeyler.length !== 0 && cografiDuzeyler.map(duzey =>({label:duzey.adi, value:duzey.id}))
+  const cografiDuzeyOption = cografiDuzeyler && cografiDuzeyler.length !== 0 && cografiDuzeyler.map(duzey => ({
+    label: duzey.adi,
+    value: duzey.id
+  }))
 
   const seciliAnketPeriyot = () => {
     if (seciliAnket && seciliAnket.periyot) return {
@@ -35,31 +40,22 @@ export default function AnketForm ({seciliAnket, history}) {
     }
     else return null
   }
-  const durumBelirleme = (seciliAnket, durum, nesne1, nesne2) => {
-    if(seciliAnket){
-      if(durum){
-        return nesne1
-      }
-      else return nesne2
-    }
-    else return nesne2
-  }
   const initialValues = {
-    kodu:seciliAnket ? seciliAnket.id :'',
-    adi: seciliAnket ? seciliAnket.adi :'',
+    kodu: seciliAnket ? seciliAnket.id : '',
+    adi: seciliAnket ? seciliAnket.adi : '',
     periyot: seciliAnketPeriyot() || null,
     birimDuzeyi: null,
-    orneklemSayisi: seciliAnket ? seciliAnket.orneklemSayisi: '',
+    orneklemSayisi: seciliAnket ? seciliAnket.orneklemSayisi : '',
     cografiDuzeyi: seciliAnketCografiDuzey() || null,
-    sema:seciliAnket ? seciliAnket.sema :'',
-    ustDurum: seciliAnket ? seciliAnket.ustDurum === 0 ? durumlar[1] : durumlar[0] : durumlar[1],
-    harzemliDurum:seciliAnket ?  durumBelirleme(seciliAnket, seciliAnket.harzemliDurum, durumlar[0], durumlar[1]) : durumlar[1],
+    sema: seciliAnket ? seciliAnket.sema : '',
+    ustDurum: seciliAnket ? seciliAnket.ustDurum !== 0 : false,
+    harzemliDurum: seciliAnket ? seciliAnket.harzemliDurum !== 0 : false,
     kontrolorSayisiBolge: '',
     anketorSayisiBolge: '',
     kontrolorSayisiMerkez: '',
     anketorSayisiMerkez: '',
     cevaplayiciBirim: null,
-    duzeltmeDurum: null,
+    duzeltmeDurum: seciliAnket ? seciliAnket.duzeltmeDurum : false
   }
 
   const handleSubmit = (event) => {
@@ -80,130 +76,111 @@ export default function AnketForm ({seciliAnket, history}) {
           <Form onSubmit={handleSubmit}>
             <Container fluid>
               <Satir>
-                <Col sm={3} md={3} lg={3}>
-                  <FormGroup label={'Anket Kodu:'}/>
+                <Col sm={5.5} md={5.5} lg={5.5}>
+                  <FormGroup label={'Anket Kodu:'}>
+                    <InputGroup large disabled={seciliAnket} name="kodu" value={values.kodu} onChange={handleChange}/>
+                  </FormGroup>
                 </Col>
-                <Col sm={6} md={6} lg={6}>
-                  <InputGroup disabled={seciliAnket} name="kodu" value={values.kodu} onChange={handleChange}/>
-                </Col>
-              </Satir>
-              <Satir>
-                <Col sm={3} md={3} lg={3}>
-                  <FormGroup label={'Anket Adı:'}/>
-                </Col>
-                <Col sm={6} md={6} lg={6}>
-                  <InputGroup name="adi" value={values.adi} onChange={handleChange}/>
+                <Col/>
+                <Col sm={5.5} md={5.5} lg={5.5}>
+                  <FormGroup label={'Anket Adı:'}>
+                    <InputGroup large name="adi" value={values.adi} onChange={handleChange}/>
+                  </FormGroup>
                 </Col>
               </Satir>
               <Satir>
-                <Col sm={3} md={3} lg={3}>
-                  <FormGroup label={'Periyodu:'} labelFor={"?"}/>
+                <Col sm={5.5} md={5.5} lg={5.5}>
+                  <FormGroup label={'Periyodu:'}>
+                    <Field name='periyot' isClearable value={values.periyot || null} options={periyotOption}
+                           component={SelectField}/>
+                  </FormGroup>
                 </Col>
-                <Col sm={6} md={6} lg={6}>
-                  <Field name='periyot' isClearable value={values.periyot || null} options={periyotOption}
-                         component={SelectField}/>
-                </Col>
-              </Satir>
-              <Satir>
-                <Col sm={3} md={3} lg={3}>
-                  <FormGroup label={'Verinin Birim Düzeyi:'}/>
-                </Col>
-                <Col sm={6} md={6} lg={6}>
-                  <Field name='birimDuzeyim' isClearable value={values.birimDuzeyi || null}
-                         component={SelectField}/>
+                <Col/>
+                <Col sm={5.5} md={5.5} lg={5.5}>
+                  <FormGroup label={'Verinin Birim Düzeyi:'}>
+                    <Field name='birimDuzeyim' isClearable value={values.birimDuzeyi || null}
+                           component={SelectField}/>
+                  </FormGroup>
                 </Col>
               </Satir>
               <Satir>
-                <Col sm={3} md={3} lg={3}>
-                  <FormGroup label={'Örneklem Boyutu:'}/>
+                <Col sm={5.5} md={5.5} lg={5.5}>
+                  <FormGroup label={'Örneklem Boyutu:'}>
+                    <InputGroup name="orneklemSayisi" value={values.orneklemSayisi} onChange={handleChange}/>
+                  </FormGroup>
                 </Col>
-                <Col sm={6} md={6} lg={6}>
-                  <InputGroup name="orneklemSayisi" value={values.orneklemSayisi} onChange={handleChange}/>
-                </Col>
-              </Satir>
-              <Satir>
-                <Col sm={3} md={3} lg={3}>
-                  <FormGroup label={'Coğrafi Düzeyi:'} labelFor={"?"}/>
-                </Col>
-                <Col sm={6} md={6} lg={6}>
-                  <Field name='cografiDuzeyi' isClearable value={values.cografiDuzeyi || null} options={cografiDuzeyOption}
-                         component={SelectField}/>
+                <Col/>
+                <Col sm={5.5} md={5.5} lg={5.5}>
+                  <FormGroup label={'Coğrafi Düzeyi:'}>
+                    <Field name='cografiDuzeyi' isClearable value={values.cografiDuzeyi || null}
+                           options={cografiDuzeyOption}
+                           component={SelectField}/>
+                  </FormGroup>
                 </Col>
               </Satir>
               <Satir>
-                <Col sm={3} md={3} lg={3}>
-                  <FormGroup label={'Şeması:'}/>
-                </Col>
-                <Col sm={6} md={6} lg={6}>
-                  <InputGroup name="sema" value={values.sema} onChange={handleChange}/>
-                </Col>
-              </Satir>
-              <Satir>
-                <Col sm={3} md={3} lg={3}>
-                  <FormGroup label={'ÜST de var mı?'}/>
-                </Col>
-                <Col sm={6} md={6} lg={6}>
-                  <Field name='ustDurum' isClearable value={values.ustDurum || null} options={durumlar}
-                         component={SelectField}/>
+                <Col sm={5.5} md={5.5} lg={5.5}>
+                  <FormGroup label={'Şeması:'}>
+                    <InputGroup name="sema" value={values.sema} onChange={handleChange}/>
+                  </FormGroup>
                 </Col>
               </Satir>
               <Satir>
-                <Col sm={3} md={3} lg={3}>
-                  <FormGroup label={'Harzemli de var mı?'}/>
-                </Col>
-                <Col sm={6} md={6} lg={6}>
-                  <Field name='harzemliDurum' isClearable value={values.harzemliDurum || null} options={durumlar}
-                         component={SelectField}/>
+                <Col sm={5.5} md={5.5} lg={5.5}>
+                  <Switch name="ustDurum" label='ÜST de var mı?' alignIndicator='right'
+                          checked={values.ustDurum} onChange={handleChange}/>
                 </Col>
               </Satir>
               <Satir>
-                <Col sm={3} md={3} lg={3}>
-                  <FormGroup label={'ÇALIŞMA DA YER ALAN TOPLAM KONTROLÖR SAYISI - MERKEZ:'}/>
-                </Col>
-                <Col sm={6} md={6} lg={6}>
-                  <InputGroup type="number" name="kontrolorSayisiMerkez" value={values.kontrolorSayisiMerkez} onChange={handleChange}/>
+                <Col sm={5.5} md={5.5} lg={5.5}>
+                  <Switch name="harzemliDurum" label='Harzemli de var mı?' alignIndicator='right'
+                          checked={values.harzemliDurum} onChange={handleChange}/>
                 </Col>
               </Satir>
               <Satir>
-                <Col sm={3} md={3} lg={3}>
-                  <FormGroup label={'ÇALIŞMA DA YER ALAN TOPLAM ANKETÖR SAYISI - MERKEZ:'}/>
+                <Col sm={5.5} md={5.5} lg={5.5}>
+                  <FormGroup label={'Çalışma da yer alan toplam kontrolör sayısı - MERKEZ:'}>
+                    <InputGroup type="number" name="kontrolorSayisiMerkez" value={values.kontrolorSayisiMerkez}
+                                onChange={handleChange}/>
+                  </FormGroup>
                 </Col>
-                <Col sm={6} md={6} lg={6}>
-                  <InputGroup type="number" name="anketorSayisiMerkez" value={values.anketorSayisiMerkez} onChange={handleChange}/>
-                </Col>
-              </Satir>
-              <Satir>
-                <Col sm={3} md={3} lg={3}>
-                  <FormGroup label={'ÇALIŞMA DA YER ALAN TOPLAM KONTROLÖR SAYISI - BÖLGE:'}/>
-                </Col>
-                <Col sm={6} md={6} lg={6}>
-                  <InputGroup type="number" name="kontrolorSayisiBolge" value={values.kontrolorSayisiBolge} onChange={handleChange}/>
-                </Col>
-              </Satir>
-              <Satir>
-                <Col sm={3} md={3} lg={3}>
-                  <FormGroup label={'ÇALIŞMA DA YER ALAN TOPLAM ANKETÖR SAYISI - BÖLGE:'}/>
-                </Col>
-                <Col sm={6} md={6} lg={6}>
-                  <InputGroup type="number" name="anketorSayisiBolge" value={values.anketorSayisiBolge} onChange={handleChange}/>
+                <Col/>
+                <Col sm={5.5} md={5.5} lg={5.5}>
+                  <FormGroup label={'Çalışma da yer alan toplam anketör sayısı - MERKEZ:'}>
+                    <InputGroup type="number" name="anketorSayisiMerkez" value={values.anketorSayisiMerkez}
+                                onChange={handleChange}/>
+                  </FormGroup>
                 </Col>
               </Satir>
               <Satir>
-                <Col sm={3} md={3} lg={3}>
-                  <FormGroup label={'Mevsimsel Düzeltme Kullanılıyor mu?'}/>
+                <Col sm={5.5} md={5.5} lg={5.5}>
+                  <FormGroup label={'Çalışma da yer alan toplam kontrolör sayısı - BÖLGE:'}>
+                    <InputGroup type="number" name="kontrolorSayisiBolge" value={values.kontrolorSayisiBolge}
+                                onChange={handleChange}/>
+                  </FormGroup>
                 </Col>
-                <Col sm={6} md={6} lg={6}>
-                  <Field name='duzeltmeDurum' isClearable value={values.duzeltmeDurum || null} options={durumlar}
-                         component={SelectField}/>
+                <Col/>
+                <Col sm={5.5} md={5.5} lg={5.5}>
+                  <FormGroup label={'Çalışma da yer alan toplam anketör sayısı - BÖLGE:'}>
+                    <InputGroup type="number" name="anketorSayisiBolge" value={values.anketorSayisiBolge}
+                                onChange={handleChange}/>
+                  </FormGroup>
                 </Col>
               </Satir>
+              <Satir>
+                <Col sm={5.5} md={5.5} lg={5.5}>
+                  <Switch name="duzeltmeDurum" label='Mevsimsel Düzeltme Kullanılıyor mu?' alignIndicator='right'
+                          checked={values.duzeltmeDurum} onChange={handleChange}/>
+                </Col>
+              </Satir>
+              <Satir/>
               <Satir>
                 <Col sm={8} md={8} lg={8}/>
                 <Col>
-                  <Button fill intent='danger' text={"Geri Dön"} onClick={history.goBack}/>
+                  <Button fill intent='danger' text={'Geri Dön'} onClick={history.goBack}/>
                 </Col>
                 <Col>
-                  <Button fill intent='success' text={"Kaydet"}/>
+                  <Button fill intent='success' text={'Kaydet'}/>
                 </Col>
               </Satir>
             </Container>
