7fac590642efb789ec02de7ef7a489932cffec72
Selçuk Kurak <slckkrk1996@gmail.com>
2021-03-02 16:38:37 +0300

    react-query provider route'ların başına koyuldu.

--- src/App.js
+++ src/App.js
@@ -5,22 +5,24 @@ import BultenLoader from './bilesenler/loader/BultenLoader'
 import ReferansLoader from './bilesenler/loader/ReferansLoader'
 import IdariKayitLoader from './bilesenler/loader/IdariKayitLoader'
 import BirimLoader from './bilesenler/loader/BirimLoader'
-import {BrowserRouter as Router, Route, Switch,Redirect} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import styled from 'styled-components'
 import { Colors } from '@blueprintjs/core'
 import HeaderBar from './bilesenler/HeaderBar'
 import YanMenuItem from './bilesenler/yan-menu/YanMenu'
-import {ArayuzRenkleri} from '@tuik/renkler'
-import { IdariKayitSayfa, AnketSayfa, UrunSayfa , BultenSayfa} from './bilesenler/sayfalar'
+import { ArayuzRenkleri } from '@tuik/renkler'
+import { IdariKayitSayfa, AnketSayfa, UrunSayfa, BultenSayfa } from './bilesenler/sayfalar'
 import GlobalStyle from './bilesenler/globalStiller'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 const Wrapper = styled.div`
-    padding: 0;
+  padding: 0;
 `
 const YanMenu = styled.div`
   width: 210px;
   height: 100vh;
-  top:0;
-  left:0;
+  top: 0;
+  left: 0;
   position: fixed;
   overflow-y: auto;
   background-color: ${ArayuzRenkleri.yanMenu};
@@ -30,7 +32,9 @@ const OrtaBolme = styled.div`
   margin-left: 210px;
   height: 100vh;
 `
-function App() {
+
+const queryClient = new QueryClient();
+function App () {
   return (
     <Router>
       <RecoilRoot>
@@ -40,28 +44,29 @@ function App() {
         <ReferansLoader/>
         <IdariKayitLoader/>
         <BirimLoader/>
-        <Wrapper>
-          <HeaderBar/>
-          <GlobalStyle/>
-          <YanMenu>
-            <YanMenuItem/>
-          </YanMenu>
-          <OrtaBolme>
-            <Switch>
-              <Redirect exact from={"/"} to={"/urunler"}/>
-              <Route path={"/urunler"} component={UrunSayfa}/>
+        <QueryClientProvider client={queryClient}>
+          <Wrapper>
+            <HeaderBar/>
+            <GlobalStyle/>
+            <YanMenu>
+              <YanMenuItem/>
+            </YanMenu>
+            <OrtaBolme>
+              <Switch>
+                <Redirect exact from={'/'} to={'/urunler'}/>
+                <Route path={'/urunler'} component={UrunSayfa}/>
 
-              <Route path={"/idari-kayitlar"} component={IdariKayitSayfa}/>
+                <Route path={'/idari-kayitlar'} component={IdariKayitSayfa}/>
 
-              <Route path={"/haber-bultenleri"} component={BultenSayfa}/>
-              <Route path={"/anketler"} component={AnketSayfa}/>
-            </Switch>
-          </OrtaBolme>
-        </Wrapper>
+                <Route path={'/haber-bultenleri'} component={BultenSayfa}/>
+                <Route path={'/anketler'} component={AnketSayfa}/>
+              </Switch>
+            </OrtaBolme>
+          </Wrapper>
+        </QueryClientProvider>
       </RecoilRoot>
     </Router>
-  );
+  )
 }
 
-
-export default App;
+export default App
