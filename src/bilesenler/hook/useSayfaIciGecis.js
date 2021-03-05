ab4c52e94f91a1f7d553fbc0060d60b9147265f1
Selçuk Kurak <slckkrk1996@gmail.com>
2021-03-05 11:42:52 +0300

    idari kayıtlar içinde form sayfa geçis sistemi sağlandı.

--- src/bilesenler/hook/useSayfaIciGecis.js
+++ src/bilesenler/hook/useSayfaIciGecis.js
@@ -4,12 +4,24 @@ export default function useSayfaIciGecis() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [genel, setGenel] = useState(true)
 
+  const [idariGenel, setIdariGenel] = useState(true)
+
+  const [idariTablo, setIdariTablo] = useState(false)
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [cikti, setCikti] = useState(false)
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [girdi, setGirdi] = useState(false)
 
+  const genelIdariSayfaClick = () => {
+    setIdariGenel(true)
+    setIdariTablo(false)
+  }
+
+  const tabloIdariSayfaClick = () => {
+    setIdariGenel(false)
+    setIdariTablo(true)
+  }
   const genelSayfaClick = () => {
     setGenel(true)
     setCikti(false)
@@ -28,12 +40,16 @@ export default function useSayfaIciGecis() {
     setCikti(false)
   }
 
-  return [
+  return {
     genel,
     cikti,
     girdi,
+    idariGenel,
+    idariTablo,
+    genelIdariSayfaClick,
+    tabloIdariSayfaClick,
     genelSayfaClick,
     ciktiSayfaClick,
     girdiSayfaClick,
-  ]
+  }
 }
