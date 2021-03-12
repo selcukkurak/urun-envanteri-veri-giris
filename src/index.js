30cacf5ba3e934b7b0883cde73112b471ed22d62
Selçuk Kurak <slckkrk1996@gmail.com>
2021-03-12 15:00:06 +0300

    axios configure edildi.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import setupAxiosInterceptors from './bilesenler/setupAxiosInterceptors'
+
+setupAxiosInterceptors()
 
 ReactDOM.render(
   <React.StrictMode>
