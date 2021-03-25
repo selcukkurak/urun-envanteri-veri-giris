ca5b8193ce2f2fc6dab7f729d251f2f29021b6d9
Selçuk Kurak <slckkrk1996@gmail.com>
2021-03-25 10:22:55 +0300

    tablo ekleme formları eklendi.

---
+++ src/bilesenler/form/idari-kayit-tablo/TabloEklemeForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TabloForm from './TabloForm'
+
+
+export default function TabloEklemeForm({history}){
+
+  return(
+    <TabloForm
+      history={history}
+    />
+  )
+}
