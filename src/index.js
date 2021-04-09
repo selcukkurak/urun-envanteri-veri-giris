2cd49a258d835b13989d43487fafd3796df1b6cc
Selçuk Kurak <slckkrk1996@gmail.com>
2021-04-09 15:29:21 +0300

    setup axios kaldırıldı.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import setupAxiosInterceptors from './bilesenler/setupAxiosInterceptors'
-
-setupAxiosInterceptors()
 
 ReactDOM.render(
   <React.StrictMode>
